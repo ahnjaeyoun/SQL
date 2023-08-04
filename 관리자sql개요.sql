8b50027eb3c85a4fb9b88701aed17b3f8c0ccb62
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-08-04 15:48:17 +0900

    2023 08 04

--- 관리자sql개요.sql
+++ 관리자sql개요.sql
@@ -17,3 +17,20 @@ GRANT RESOURCE, CONNECT TO kh;
 
 -- 객체가 생성될 수 있는 공간 할당량 지정
 ALTER USER kh DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+
+-----------------------------------------------------------------------------------------
+
+
+-- 새 사용자 계정 생성 구문 오류 발생 시
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 새로운 사용자 계정 생성 (sys as sysdba 으로 진행)
+CREATE USER workbook IDENTIFIED BY workbook;
+
+
+-- 사용자 계정 권한 부여 설정
+GRANT RESOURCE, CONNECT TO workbook;
+
+-- 객체가 생성될 수 있는 공간 할당량 지정
+ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
